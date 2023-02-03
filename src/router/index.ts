6a7a1fe8202a1e66ad1c71774fb1d5d57367b57f
-yayabb <2286834433@qq.com>
2023-02-03 21:44:01 +0800

    feat: 🎸 初始化项目

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,7 +12,7 @@ const routes: Array<RouteRecordRaw> = [
     name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    // which is lazy-loaded when the route is visited.懒加载
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue")
   }
