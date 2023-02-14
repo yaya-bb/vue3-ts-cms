d438c6f21ef5596232673a28d3c1f977e911e2a4
-yayabb <2286834433@qq.com>
2023-02-14 20:38:34 +0800

    style: 💄 样式修改

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: -yayabb 2286834433@qq.com
- * @Date: 2023-02-02 11:52:10
- * @LastEditors: -yayabb 2286834433@qq.com
- * @LastEditTime: 2023-02-14 20:33:33
- * @FilePath: \vue3-ts-cms\src\views\HomeView.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
   <div class="home">This is the home.</div>
 </template>
