b5d473909ace4949ff2b4707381eae464181910b
-yayabb <2286834433@qq.com>
2023-02-26 23:37:51 +0800

    feat: 🎸 添加导航守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@
  * @Author: -yayabb 2286834433@qq.com
  * @Date: 2023-02-02 11:52:11
  * @LastEditors: -yayabb 2286834433@qq.com
- * @LastEditTime: 2023-02-25 20:40:11
+ * @LastEditTime: 2023-02-26 23:36:02
  * @FilePath: \vue3-ts-cms\src\router\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -40,6 +40,11 @@ router.beforeEach((to) => {
     const token = LocalCache.getCache('token');
     if (!token) {
       return '/login';
+    }
+  }
+  if (to.path.indexOf('/main') !== -1) {
+    if (to.name === 'notFound') {
+      to.name = 'user';
     }
   }
 });
