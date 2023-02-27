e635d522d99245bc68fd022e1dd898cbb186d1da
-yayabb <2286834433@qq.com>
2023-02-27 09:32:26 +0800

    feat: 🎸 默认加载所有的routes

--- src/utils/map-menus.ts
+++ src/utils/map-menus.ts
@@ -2,7 +2,7 @@
  * @Author: -yayabb 2286834433@qq.com
  * @Date: 2023-02-25 21:47:46
  * @LastEditors: -yayabb 2286834433@qq.com
- * @LastEditTime: 2023-02-27 09:06:29
+ * @LastEditTime: 2023-02-27 09:31:39
  * @FilePath: \vue3-ts-cms\src\utils\map-menus.ts
  * @Description: 对Menus做映射，转成其他东西
  */
@@ -13,11 +13,16 @@ export function mapMenusToRoutes(userMenus: any[]): RouteRecordRaw[] {
   const routes: RouteRecordRaw[] = [];
   // 1. 先去加载默认所有的routes
   const allRoutes: RouteRecordRaw[] = [];
-  // 动态加载
-  // webpack函数:去'../router/main'中匹配所有ts文件;true则代表递归查找
+  // 动态加载-获取路由对象
+  // 去'../router/main'中匹配所有ts文件;true则代表递归查找
   const routeFiles = require.context('../router/main', true, /\.ts/);
+  // webpack函数
   routeFiles.keys().forEach((key) => {
-    console.log(key);
+    // ./system/user/user.ts
+    // 对路径进行处理然后拼接
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const route = require('../router/main' + key.split('.')[1]);
+    allRoutes.push(route.default);
   });
   // 2. 根据菜单获取需要添加的routes
   return routes;
