3d5df971f0ca07cccce27b884ad1838391df18d8
-yayabb <2286834433@qq.com>
2023-03-04 10:36:54 +0800

    feat: 🎸 page-search组件的封装

---
+++ src/components/page-search/src/page-search.vue
@@ -0,0 +1,58 @@
+<template>
+  <!-- 将formData传到表单 -->
+  <!-- v-model组件实现双向绑定，而传过去的属性就是modelValue而不是formData -->
+  <div class="page-search">
+    <my-form v-bind="searchFormConfig" v-model="formData">
+      <template #header>
+        <h1 class="header">高级检索</h1>
+      </template>
+      <template #footer>
+        <div class="handle-btns">
+          <el-button icon="el-icon-refresh">重置</el-button>
+          <el-button type="primary" icon="el-icon-search">搜索</el-button>
+        </div>
+      </template>
+    </my-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import MyForm from '@/base-ui/form';
+
+export default defineComponent({
+  props: {
+    searchFormConfig: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    MyForm
+  },
+  setup() {
+    const formData = ref({
+      id: '',
+      name: '',
+      password: '',
+      sport: '',
+      createTime: ''
+    });
+
+    return {
+      formData
+    };
+  }
+});
+</script>
+
+<style scoped>
+.header {
+  color: red;
+}
+.handle-btns {
+  text-align: right;
+  /* margin具有穿透性 */
+  padding: 0 50px 20px 0;
+}
+</style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -2,60 +2,41 @@
  * @Author: -yayabb 2286834433@qq.com
  * @Date: 2023-02-23 18:29:06
  * @LastEditors: -yayabb 2286834433@qq.com
- * @LastEditTime: 2023-03-03 12:50:16
+ * @LastEditTime: 2023-03-04 10:33:27
+ * @FilePath: \vue3-ts-cms\src\views\main\system\user\user.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<!--
+ * @Author: -yayabb 2286834433@qq.com
+ * @Date: 2023-02-23 18:29:06
+ * @LastEditors: -yayabb 2286834433@qq.com
+ * @LastEditTime: 2023-03-04 10:28:29
  * @FilePath: \vue3-ts-cms\src\views\main\system\user\user.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div class="user">
-    <!-- 将formData传到表单 -->
-    <!-- v-model组件实现双向绑定，而传过去的属性就是modelValue而不是formData -->
-    <my-form v-bind="searchFormConfig" v-model="formData">
-      <template #header>
-        <h1 class="header">高级检索</h1>
-      </template>
-      <template #footer>
-        <div class="handle-btn">
-          <el-button>重置</el-button>
-          <el-button type="primary">搜索</el-button>
-        </div>
-      </template>
-    </my-form>
+    <!-- 配置传给给page-search组件 -->
+    <page-search :searchFormConfig="searchFormConfig"/>
     <div class="content"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import MyForm from '@/base-ui/form';
-
+import { defineComponent } from 'vue';
+import PageSearch from '@/components/page-search';
 import { searchFormConfig } from './config/search.config';
-
 export default defineComponent({
   name: 'user',
   components: {
-    MyForm
+    PageSearch
   },
   setup() {
-    // 数据复杂，是一个对象
-    const formData = ref({
-      id: '',
-      name: '',
-      password: '',
-      sport: '',
-      createTime: ''
-    });
     return {
-      formData,
       searchFormConfig
     };
   }
 });
 </script>
 
-<style scoped>
-.handle-btn {
-  text-align: right;
-  padding: 2px 50px 20px 5px;
-}
-</style>
+<style scoped></style>
