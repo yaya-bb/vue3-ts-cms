41aa5da32d8365161bfb57ecede43d5107d9ff15
-yayabb <2286834433@qq.com>
2023-03-08 23:49:11 +0800

    test: 💍 hooks:use-permission

--- src/hooks/use-permission.ts
+++ src/hooks/use-permission.ts
@@ -6,7 +6,7 @@
  * @FilePath: \vue3-ts-cms\src\hooks\use-permission.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { useStore } from '@/store'
+import { useStore } from '@/store';
 
 export function usePermission(pageName: string, hanldeName: string) {
   const store = useStore();
