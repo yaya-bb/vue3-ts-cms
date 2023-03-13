75abf4b8a0fd09e47a86f66c27186cf7fd3c3995
-yayabb <2286834433@qq.com>
2023-03-13 09:49:50 +0800

    feat: 🎸 更新分页器的数据

--- src/base-ui/table/src/table.vue
+++ src/base-ui/table/src/table.vue
@@ -2,7 +2,7 @@
  * @Author: -yayabb 2286834433@qq.com
  * @Date: 2023-03-05 19:43:27
  * @LastEditors: -yayabb 2286834433@qq.com
- * @LastEditTime: 2023-03-07 20:48:10
+ * @LastEditTime: 2023-03-13 09:45:38
  * @FilePath: \vue3-ts-cms\src\base-ui\table\src\table.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -59,10 +59,10 @@
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
           :current-page="page.currentPage"
-          :page-sizes="[100, 200, 300, 400]"
           :page-size="page.pageSize"
+          :page-sizes="[10, 20, 30]"
           layout="total, sizes, prev, pager, next, jumper"
-          :total="400"
+          :total="listCount"
         >
         </el-pagination>
       </slot>
