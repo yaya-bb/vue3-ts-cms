3f747cfb2208c3eae1477e4ed03701c0661f98e1
-yayabb <2286834433@qq.com>
2023-03-16 14:36:23 +0800

    feat: 🎸 base-echart和role修改

--- src/base-ui/echart/src/base-echart.vue
+++ src/base-ui/echart/src/base-echart.vue
@@ -3,7 +3,7 @@
  * @Author: -yayabb 2286834433@qq.com
  * @Date: 2023-03-15 15:02:11
  * @LastEditors: -yayabb 2286834433@qq.com
- * @LastEditTime: 2023-03-15 16:54:10
+ * @LastEditTime: 2023-03-16 14:31:41
  * @FilePath: \vue3-ts-cms\src\base-ui\echart\src\base-echart.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
